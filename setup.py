c3dce66ef170932b0a5ee236c16523f60695769e
isham <ishammohamed@outlook.com>
2025-04-17 10:43:37 +0100

    updated minimum python version requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fast_stringcase",
-    version="1.2.0b1",
+    version="1.2.0b2",
     description="C extension for string case conversions. The logic is ported from and based on stringcase == 1.2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -27,5 +27,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
