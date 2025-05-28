0c63b3740436950f6f1817ac85cba7cb2549d8ae
isham <ishammohamed@outlook.com>
2025-05-28 04:05:33 +0100

    added pyproject.toml

--- setup.py
+++ setup.py
@@ -9,27 +9,7 @@ module = Extension(
     include_dirs=include_dirs,
 )
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="fast_stringcase",
-    version="1.2.0b4",
-    description="C extension for string case conversions. The logic is ported from and based on stringcase == 1.2.0",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Isham Mohamed",
-    author_email="isham.iqbal@icloud.com",
-    license='MIT',
-    url='https://github.com/IshamMohamed/fast-stringcase/',
-    download_url='https://pypi.org/project/fast-stringcase/',
     ext_modules=[module],
-    packages=["fast_stringcase"],
-    package_data={'fast_stringcase': ['fast_stringcase.pyi']},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.9",
 )
